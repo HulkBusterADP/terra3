3937fda36a48f2b970b6392285f31b5a1ea9bcb0
Venna12 <52368332+Venna12@users.noreply.github.com>
2023-03-28 18:35:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIA2TJD4BE6IZNPXTDK"
-secret_key = "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"
+access_key = "AKIA2TJD4BE6E63Q3M3P"
+secret_key = "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"
 }
 
 resource "aws_instance" "web" {
